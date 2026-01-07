337c17db103985773ea7dd3fa6afa481e0106dc8
Alan Soto <alansoto121298@gmail.com>
2026-01-07 10:17:14 -0600

    declaracion de variables a trabajar Js

---
+++ script.js
@@ -0,0 +1,4 @@
+//Aqui declaramos las 3 variables a trabajar
+const chat = document.querySelector("#chat");
+const input = document.querySelector("#messageInput");
+const boton = document.querySelector("#sendBtn");

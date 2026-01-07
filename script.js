8b7c976c2f9b5f17343250cda159d14131c15d9c
Alan Soto <alansoto121298@gmail.com>
2026-01-07 10:21:51 -0600

    Funcion para agregar msg al chat JS

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 const chat = document.querySelector("#chat");
 const input = document.querySelector("#messageInput");
 const boton = document.querySelector("#sendBtn");
+
+//Funcion para agregar el msg al chat
+ function agregarMensaje(text,ClassName){
+    const div = document.createElement("div");  // Creamos un elemento "DIV"
+    div.textContent = text;  // El div va ser igual al texto
+    div.ClassName = ClassName;  //Agregamos clase al bott
+    chat.appendChild("div");  // hacemos que el chat sea un hijo
+    chat.scrollTop = chat.scrollHeight;  // Hacemos que tenga scroll el chat
+
+ }
